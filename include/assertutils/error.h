df8516913f92c851f9313a8521b0fa7f447cbdac
tsukigva <tsukigva@gmail.com>
2024-06-24 14:14:56 -0300

    improving messages

--- include/assertutils/error.h
+++ include/assertutils/error.h
@@ -10,33 +10,45 @@ extern "C" {
 void log_ok(const char* msg, const char* fn, const char* file, int line);
 void log_status(const char* msg, const char* fn, const char* file, int line);
 
+void log_okf(const char* fn, const char* file, int line, const char* msg, ...);
+void log_statusf(const char* fn, const char* file, int line, const char* msg, ...);
+
 #ifndef __RELEASE__
 void log_err(const char* msg, const char* fn, const char* file, int line);
 void log_warn(const char* msg, const char* fn, const char* file, int line);
 
 void log_errf(const char* fn, const char* file, int line, const char* msg, ...);
+void log_warnf(const char* fn, const char* file, int line, const char* msg, ...);
 #endif
 
-void log_warnf(const char* fn, const char* file, int line, const char* msg, ...);
-
 #ifndef __RELEASE__
+
 #define ERR(msg) log_err(msg, __func__, __FILE__, __LINE__)
 #define WARN(msg) log_warn(msg, __func__, __FILE__, __LINE__)
 
 #define NICE(msg) log_ok(msg, __func__, __FILE__, __LINE__)
 #define INFO(msg) log_status(msg, __func__, __FILE__, __LINE__)
 
+#define NICEF(msg, ...) log_okf(__func__, __FILE__, __LINE__, msg, __VA_ARGS__)
+#define INFOF(msg, ...) log_statusf(__func__, __FILE__, __LINE__, msg, __VA_ARGS__)
+
 #define ERRF(msg, ...) log_errf(__func__, __FILE__, __LINE__, msg, __VA_ARGS__)
 #define WARNF(msg, ...) log_warnf(__func__, __FILE__, __LINE__, msg, __VA_ARGS__)
+
 #else
+
 #define ERR(msg) do{}while(0)
 #define WARN(msg) do{}while(0)
 
-#define NICE(msg) log_ok(msg, __func__, "?", -1)
-#define INFO(msg) log_status(msg, __func__, "?", -1)
+#define NICE(msg) log_ok(msg, __func__, "?", 0)
+#define INFO(msg) log_status(msg, __func__, "?", 0)
+
+#define NICEF(msg) log_okf(__func__, "?", 0, msg, __VA_ARGS__)
+#define INFOF(msg) log_statusf(__func__, "?", 0, msg, __VA_ARGS__)
 
 #define ERRF(fmt) do{}while(0)
 #define WARNF(fmt) do{}while(0)
+
 #endif
 
 
--- src/assertutils/error.c
+++ src/assertutils/error.c
@@ -3,12 +3,38 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+// TODO: Centralize?
+// general function f(color, msg, file, line) which handles each case
+
 void log_ok(const char* msg, const char* fn, const char* file, int line) {
 	printf("\033[32;1mGOOD\t\033[0m\033[1m%.10s\t:%d [\033[32;1m%s\033[0m\033[1m] => \033[32;1m'%s'\033[0m\n", file, line, fn, msg);
 }
 
 void log_status(const char* msg, const char* fn, const char* file, int line) {
 	printf("\033[1mINFO\t\033[0m\033[1m%.10s\t:%d [\033[32;1m%s\033[0m\033[1m] => \033[36;1m'%s'\033[0m\n", file, line, fn, msg);
+}
+
+void log_okf(const char* fn, const char* file, int line, const char* msg, ...) {
+	printf("\033[32;1mGOOD\t\033[0m\033[1m%.10s\t:%d [\033[32;1m%s\033[0m\033[1m] => \033[32;1m'", file, line, fn);
+
+	va_list args;
+	va_start(args, msg);
+	vfprintf(stderr, msg, args);
+	va_end(args);
+
+	puts("\033[0m'");
+}
+
+void log_statusf(const char* fn, const char* file, int line, const char* msg, ...) {
+	printf("\033[36;1mINFO\t\033[0m\033[1m%.10s\t:%d [\033[32;1m%s\033[0m\033[1m] => \033[36;1m'", file, line, fn);
+
+	va_list args;
+	va_start(args, msg);
+	vprintf(msg, args);
+	va_end(args);
+
+	puts("\033[0m");
+
 }
 
 #ifndef __RELEASE__
